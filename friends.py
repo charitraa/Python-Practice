19de83a2f8f3c6f853b152236d3b8e99e72a915a
Your Name <you@example.com>
2023-08-14 13:46:39 +0545

    nnew

---
+++ friends.py
@@ -0,0 +1,15 @@
+var1 = "ravi"
+var2 = "chari"
+var3 = "character"
+var4 = "Jesish"
+var5 = "Minal"
+var6 = "pratik"
+
+
+print(var1)
+print(var2)
+print(var3)
+print(var3)
+print(var4)
+print(var5)
+print(var6)
