6615e2be52333eda3d03185dafa5d17cf8b9de36
Charitra Shrestha <stharabi9862187405@gmail.com>
2023-08-18 21:58:54 +0545

    update

---
+++ 100_Days/Day18-While-loop.py
@@ -0,0 +1,12 @@
+i = 0
+
+while (i <= 3):
+    print("Hello World")  # prints Hello world 4 times
+    i += 1
+
+
+a = 0
+while (a <= 100):
+
+    a = int(input("enter the number:"))
+    print(a)
---
+++ 100_Days/Day19-break and continue.py
@@ -0,0 +1,6 @@
+for i in range(10):
+    if (i == 5):
+        continue
+    elif (i == 3):
+        break
+    print(i)
