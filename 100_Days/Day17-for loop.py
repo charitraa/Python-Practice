76e16d90d806c959720b7e91f39d26285602c70e
Charitra Shrestha <stharabi9862187405@gmail.com>
2023-08-18 18:03:21 +0545

    update

---
+++ 100_Days/Day17-for loop.py
@@ -0,0 +1,20 @@
+name = "Charitra"
+
+for i in name:
+    print(i)
+    if (i == "i"):
+        print("i think you r not my name")
+
+
+color = ["red", "blue", "green"]
+for i in color:
+    print(i)
+    for i in i:
+        print(i)
+
+for i in range(1, 9):
+    print(i)
+
+
+for i in range(0, 101, 10):
+    print(i)
