17bb53711703747d39c3f6e6098adbe413a33ade
Charitra Shrestha <stharabi9862187405@gmail.com>
2023-08-19 12:07:05 +0545

    update

---
+++ 100_Days/Day20-Function.py
@@ -0,0 +1,27 @@
+a = 5
+b = 4
+
+# function
+
+
+def cal(a, b):
+    sum = (a+b)/(a*b)
+    print(sum)
+
+
+def isGreat(a, b):
+    if (a < b):
+        print(a, " is grater number")
+    else:
+        print(b, "is grater number ")
+
+
+def call():
+    pass  # just continue another line
+
+
+cal(9, 8)
+isGreat(9, 8)
+
+cal(8, 7)
+isGreat(1, 3)
