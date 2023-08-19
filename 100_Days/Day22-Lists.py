c34703964b68f655b21ccf3a49c095b155848d3b
Charitra Shrestha <stharabi9862187405@gmail.com>
2023-08-19 15:08:04 +0545

    new

---
+++ 100_Days/Day22-Lists.py
@@ -0,0 +1,15 @@
+l = [1, 2, 3]
+print(l)
+
+print(l[0])
+
+m = ["uyyfi", 66, True]
+print(m)
+
+
+if "ha" in "harry":
+    print("yes")
+else:
+    pass
+
+print(l[:3:2])  # jump index
---
+++ 100_Days/Day23-List method.py
@@ -0,0 +1,29 @@
+a = [11, 72, 33, 77, 66, 55, 98, 37, 48, 36]
+# b = a.sort()  # ascending order
+# # sort(reverse=true) descending order
+
+# print(a)
+# c = a.reverse()  # reversing the number
+
+# print(a)
+# d = a.append(12)  # add the end of index
+
+# print(a)
+# e = a.insert(0, 11)  # add the position of index
+
+# print(a)
+# f = a.pop(11)  # remove the position of index
+# print(a)
+g = a.remove(11)  # remove the elements
+print(a)
+
+# print(a.index(11))
+
+# print("-----------------------------------")
+print(a.count(11))
+y = [22, 22, 22]
+a.extend(y)
+print(a)
+
+k = a + y
+print(k)
