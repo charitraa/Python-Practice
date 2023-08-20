b1279d33186062ada121307e48af2139779f9033
Charitra Shrestha <stharabi9862187405@gmail.com>
2023-08-20 16:28:34 +0545

    new

---
+++ 100_Days/Day36-Exception handling.py
@@ -0,0 +1,29 @@
+from cmath import e
+
+
+a = input("enter the number: ")
+print(f"multiplication table of {a} is:")
+
+try:
+
+    for i in range(1, 11):
+        print(f"{int(a)}x{i}={int(a)*i}")
+
+
+except Exception as e:
+    print('error:', e)
+
+
+# except IndexError as a:
+    # print("index out of bound ", {a})
+
+try:
+    f = open("demofile.txt")
+    try:
+        f.write("Lorum Ipsum")
+    except:
+        print("Something went wrong when writing to the file")
+    finally:
+        f.close()
+except:
+    print("Something went wrong when opening the file")
