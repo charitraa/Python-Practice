2f27d46527c570ddab423b3847ceafb52302b3cc
Charitra Shrestha <stharabi9862187405@gmail.com>
2023-08-20 14:30:09 +0545

    new

--- 3.py
+++ 3.py
@@ -1,3 +1,8 @@
 print("Subscribe and Like")
 # ;ofiheolief;oiefe
 # comment is this
+print(
+    "{:<4} {:<100} {:<20} {:<12} {:<8}".format(
+        "No.", "Equipment", "Brand", "Price ($)", "Quantity"
+    )
+)
