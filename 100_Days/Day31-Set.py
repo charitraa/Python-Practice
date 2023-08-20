9ef120f5a29d46775b57e81351abc5efb5621c3c
Charitra Shrestha <stharabi9862187405@gmail.com>
2023-08-20 14:36:59 +0545

    new

---
+++ 100_Days/Day31-Set.py
@@ -0,0 +1,11 @@
+s = {1, 2, 34, 4, 5, 6, 7, 8, 9, 0, }
+print(s)
+# cannot change the set
+# similiar vaalue contain one value
+
+info = {"carla", 19, False, 5.9, 19}
+print(info)
+
+
+oe = {}  # set{ }
+print(type(oe))
