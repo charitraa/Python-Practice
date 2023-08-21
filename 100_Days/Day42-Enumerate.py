d65017e80333b9c65e5a2a6578c6524488f109eb
Charitra Shrestha <stharabi9862187405@gmail.com>
2023-08-21 11:29:09 +0545

    new

---
+++ 100_Days/Day42-Enumerate.py
@@ -0,0 +1,7 @@
+marks = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+for index, mark in enumerate(marks, 1):
+    # for index, mark in enumerate(marks):
+    print(mark)
+    if (index == 3):
+        print("hii")
