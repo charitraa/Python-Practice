082423bf47684f5e45dcc1576cf89cf9aa148c60
Charitra Shrestha <stharabi9862187405@gmail.com>
2023-08-21 14:42:33 +0545

    new

--- 100_Days/Day50-read and other method.py
+++ 100_Days/Day50-read and other method.py
@@ -7,11 +7,21 @@ while True:
     # end='' is used to avoid adding a newline character at the end of each string
     print(line, end='')
 
+print("")
 f = open('100_Days/Day49-File Io/edit.txt', 'r')
+i = 0
 while True:
+    i = i+1
     # readline returns the next line from file, if it exists (returns an empty string at EOF)
     line = f.readline()
     if not line:
         break
     # end='' is used to avoid adding a newline character at the end of each string
+    m1 = int(line.split(',')[0])
+    m2 = int(line.split(',')[1])
+    m3 = int(line.split(',')[2])
+
+    print(m1)
+    print(m2)
+    print(m3)
     print(line, end='')
