aa4843b637e9bdd29c76cd825314583e1a1c219b
Charitra Shrestha <stharabi9862187405@gmail.com>
2023-08-21 12:37:55 +0545

    new

---
+++ 100_Days/Day46-os.py
@@ -0,0 +1,10 @@
+import os
+
+# if (not os.path.exists("")):
+#     os.mkdir("data")
+folders = os.listdir("100_Days/os Prctise")
+for i in folders:
+    # os.mkdir(f"100_Days/os Prctise/Day{i+1}")
+    # os.rename(f"100_Days/os Prctise/Days{i+1}",
+    #           f"100_Days/os Prctise/Days {i+1}")
+    print(i)
