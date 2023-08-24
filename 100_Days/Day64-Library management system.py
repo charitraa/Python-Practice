a67834ab195504253f4e7ff9e042550cf83d2480
Charitra Shrestha <stharabi9862187405@gmail.com>
2023-08-24 11:44:42 +0545

    new

---
+++ 100_Days/Day64-Library management system.py
@@ -0,0 +1,16 @@
+class library:
+    def __init__(self):
+        self.noBooks = 0
+        self.books = []
+
+    def addBook(self, book):
+        self.books.append(book)
+        self.noBooks = len(self.books)
+
+    def showInfo(self):
+        print(f"The library has {self.noBooks} books.")
+
+
+lib = library()
+lib.addBook("Ravi Shrestha")
+lib.showInfo()
