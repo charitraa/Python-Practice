731c4652ea615b11e4018a7d834d7e0da590b79b
Charitra Shrestha <stharabi9862187405@gmail.com>
2023-08-26 14:18:41 +0545

    new

--- 100_Days/Day87-shutil.py
+++ 100_Days/Day87-shutil.py
@@ -1,3 +1,11 @@
+import os
 import shutil
 
 shutil.copy("Day1.py", "Day1copy.py")
+
+
+# shutil.copy("main.py", "main2.py")
+# shutil.copytree(".tutorial", "mytutorial")
+# shutil.move(".tutorial/file.file", "file.file")
+# shutil.rmtree("mytutorial")
+os.remove("file.file")
