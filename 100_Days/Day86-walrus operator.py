14bfd0585a1edf0601b232552a4b7a81ddecdfc7
Charitra Shrestha <stharabi9862187405@gmail.com>
2023-08-26 13:34:33 +0545

    new

---
+++ 100_Days/Day86-walrus operator.py
@@ -0,0 +1,24 @@
+a = True
+print(a := False)
+
+numbers = [1, 2, 3, 4, 5]
+
+while (n := len(numbers)) > 0:
+    print(numbers.pop())
+
+
+# happy = False
+# print(happy)
+
+# print(happy := True)
+
+foods = list()
+while True:
+    food = input("What food do you like?: ")
+    if food == "quit":
+        break
+    foods.append(food)
+
+foods = list()
+while (food := input("What food do you like?: ")) != "quit":  # using walrus
+    foods.append(food)
--- 100_Days/tempCodeRunnerFile.py
+++
@@ -1,5 +0,0 @@
- # def __len__(self):
-    #     i = 0
-    #     for c in self.name:
-    #         i = i+1
-    #         return i
