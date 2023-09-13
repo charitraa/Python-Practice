70690ffcfdabb97dfd70ec20b6b657ded355806c
Charitra Shrestha <stharabi9862187405@gmail.com>
2023-09-13 10:30:24 +0545

    new

--- classwork/lab1.py
+++ classwork/lab1.py
@@ -1,6 +1,28 @@
+import math
+a = 5
+b = 3
+sum_result = a+b
+difference_result = a-b
+product_result = a*b
+quotient_result = a/b
+
+print("sum:", sum_result)
+print("difference:", difference_result)
+print("product:", product_result)
+print("quotient:", quotient_result)
+
+text = "Hello Python!"
+print("length:", len(text))
+print("uppercase:", text.upper())
+print("lowercase:", text.lower())
+
+z = math.factorial(4)
+print(z)
+
+
 x = 5
 y = "hello"
-z = 6z
+z = 6
 
 print(type(x))
 print(type(y))
