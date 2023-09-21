a588350999a19bbcca7e0eb624bfcead81c63c15
Charitra Shrestha <stharabi9862187405@gmail.com>
2023-09-21 13:00:45 +0545

    new

---
+++ classwork/lab2.py
@@ -0,0 +1,42 @@
+# No.2
+a = int(input("Enter first number:"))
+b = int(input("Enter second number:"))
+sum = a + b
+print("The sum of two number : ", sum)
+
+# No.3
+a = int(input("Enter first number:"))
+b = int(input("Enter second number:"))
+c = int(input("Enter third number:"))
+
+average = (a + b + c) / 3
+print("The average of three number : ", average)
+
+# No.4
+
+a = input("Enter the string: ")
+if a == "":
+    print("The string has null value")
+
+else:
+    print("The string has value")
+
+
+# No.5
+
+a = int(input("Enter the degree temperature to convert fahrenheit: "))
+b = (a*(9/5)+32)
+print(" The temperature in fahrenheit is: ", b)
+
+# No.6
+
+a = int(input("enter the base of triangle :"))
+b = int(input("Enter the height of the triangle:"))
+
+c = 0.5 * a * b
+print("The area of the triangle: ", c)
+
+# No.7
+a = int(input("enter the minutes to convert to hrs: "))
+b = a / 60
+print("The hours is ", b)
---
+++ classwork/tempCodeRunnerFile.py
@@ -0,0 +1,13 @@
+# No.2
+# a = int(input("Enter first number:"))
+# b = int(input("Enter second number:"))
+# sum = a + b
+# print("The sum of two number : ", sum)
+
+# # No.3
+# a = int(input("Enter first number:"))
+# b = int(input("Enter second number:"))
+# c = int(input("Enter third number:"))
+
+# average = (a + b + c) / 3
+# print("The average of three number : ", average)
