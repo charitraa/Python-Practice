b4e11a4bb02fcf4bb8a565180218332c11a2c2f2
Charitra Shrestha <stharabi9862187405@gmail.com>
2023-09-24 00:07:28 +0545

    new

--- GUI/1143.py
+++ GUI/1143.py
@@ -1,9 +1,12 @@
 from tkinter import *
 a = Tk()
-a.title(" ")
+a.title("Attribute")
 a.geometry("733x433")
-# Label arrtribute
+# Label Attribute
 
-b = Label(text="Welcome to Pycham", font="100")
+b = Label(text="Welcome to Pycham Welcome to PychamWelcome to  \nPychamWelcome to PychamWelcome to  \nPychamWelcome to PychamWelcome to \nPycham",
+          bg="black", foreground="white", padx=30, pady=30, font=("Arial", 20, "bold"), )
 b.pack()
+
+
 a.mainloop()
