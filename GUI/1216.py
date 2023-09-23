97bafe19f277fb7c2397148ff32d98f056a3968c
Charitra Shrestha <stharabi9862187405@gmail.com>
2023-09-24 00:30:10 +0545

    new

--- GUI/1216.py
+++ GUI/1216.py
@@ -6,8 +6,14 @@ rabi.geometry("1000x1000")
 
 # title
 rabi.title("Practise")
-# label
-rabina = Label(text="hello world")
+# frame
+f1 = Frame(rabi, bg="black", borderwidth=3, relief=SUNKEN)
 # to add the label in tkinter
-rabina.pack()
+f1.pack(side="left", fill="y", pady=200)
+f2 = Frame(rabi, bg="black", borderwidth=8, relief=SUNKEN)
+f2.pack(side="top", fill="x")
+l = Label(f1, text="Project Tkinter")
+l.pack(pady=42)
+l1 = Label(f2, text="Project Ravi")
+l1.pack(pady=42)
 rabi.mainloop()
