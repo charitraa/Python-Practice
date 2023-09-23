a5810da2c87843310c57fc13fba6dccebf42c0f6
Charitra Shrestha <stharabi9862187405@gmail.com>
2023-09-24 00:11:30 +0545

    new

--- GUI/1143.py
+++ GUI/1143.py
@@ -5,8 +5,10 @@ a.geometry("733x433")
 # Label Attribute
 
 b = Label(text="Welcome to Pycham Welcome to PychamWelcome to  \nPychamWelcome to PychamWelcome to  \nPychamWelcome to PychamWelcome to \nPycham",
-          bg="black", foreground="white", padx=30, pady=30, font=("Arial", 20, "bold"), )
-b.pack()
+          bg="black", foreground="white", padx=30, pady=30, font=("Arial", 20, "bold"), borderwidth=10, relief=SUNKEN)
+
+# side
+b.pack(side="left", anchor="se")
 
 
 a.mainloop()
