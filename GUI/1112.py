d246a6e4778bfb760130353812c6948cd2dcddf9
Charitra Shrestha <stharabi9862187405@gmail.com>
2023-09-23 23:21:43 +0545

    new

---
+++ GUI/1112.py
@@ -0,0 +1,14 @@
+from tkinter import *
+
+rabi = Tk()
+# size
+rabi.geometry("1000x1000")
+# min size
+rabi.minsize(300, 300)
+# max size
+rabi.maxsize(1200, 1200)
+# title
+rabi.title("Practise")
+# label
+rabina = Label(text="hello world")
+rabi.mainloop()
