1d7fad11f8e304004b604e9a1ad347728a7c814c
Charitra Shrestha <stharabi9862187405@gmail.com>
2023-09-25 11:57:43 +0545

    new

--- GUI/1058.py
+++ GUI/1058.py
@@ -4,15 +4,22 @@ rabi = Tk()
 rabi.geometry("655x333")
 # gui logicr
 
+
+def getValue():
+    print(aa.get())
+    print(bb.get())
+
+
 a = Label(rabi, text="Username")
 password = Label(rabi, text="Password")
 a.grid()
 password.grid(row=1)
 
-c = StringVar()
-d = StringVar()
 
-aa = Entry(rabi, textvariable=c)
-bb = Entry(rabi, textvariable=d)
+aa = Entry(rabi, textvariable=StringVar())
+bb = Entry(rabi, textvariable=StringVar())
 
+aa.grid(row=0, column=1)
+bb.grid(row=1, column=1)
+Button(text="Submit", command=getValue).grid()
 rabi.mainloop()
