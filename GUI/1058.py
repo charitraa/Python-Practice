b217697bb9169c65f43c35d353666eaaf1d96393
Charitra Shrestha <stharabi9862187405@gmail.com>
2023-09-25 11:06:47 +0545

    new

--- GUI/1058.py
+++ GUI/1058.py
@@ -2,5 +2,17 @@ from tkinter import *
 
 rabi = Tk()
 rabi.geometry("655x333")
-# gui logic
+# gui logicr
+
+a = Label(rabi, text="Username")
+password = Label(rabi, text="Password")
+a.grid()
+password.grid(row=1)
+
+c = StringVar()
+d = StringVar()
+
+aa = Entry(rabi, textvariable=c)
+bb = Entry(rabi, textvariable=d)
+
 rabi.mainloop()
