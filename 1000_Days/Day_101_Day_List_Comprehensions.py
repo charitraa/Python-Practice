dd09ded493871c6feccb124d7e35a4a1d0758903
charitraa <stharabi9862187405@gmail.com>
2024-08-05 22:01:43 +0545

    change the day

---
+++ 1000_Days/Day_101_Day_List_Comprehensions.py
@@ -0,0 +1,7 @@
+# Concept:
+# List comprehensions provide a concise way to create lists. They are more compact and readable than traditional loops and offer a way to generate lists by performing operations on each item in an existing iterable or by filtering items.
+
+# using for loops
+squares = []
+for i in range(10):
+    squares.append(i ** 2)
